325cab3b70c55c611b51c38ae7c169e3161bfb29
aishwary013 <87142879+aishwary013@users.noreply.github.com>
2021-07-12 20:09:05 +0530

    Update outlier treatment.py

--- Outliers/outlier treatment.py
+++ Outliers/outlier treatment.py
@@ -41,9 +41,23 @@ def replace_with_thresholds(dataframe, col_name, th1=0.05, th3=0.95):
         dataframe.loc[(dataframe[col_name] > up_limit), col_name] = up_limit
 
 
+# Drop outliers
+def drop_outliers(dataframe, col_name, th1=0.05, th3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name, th1, th3)
+    if low_limit > 0:
+        dataframe = dataframe[dataframe[col_name] > low_limit]
+        dataframe = dataframe[dataframe[col_name] < up_limit]
+    else:
+        dataframe = dataframe[dataframe[col_name] < up_limit]
+    return dataframe
+
+
 # Check & Replace Outliers
 for col in df.columns:
     print(col,check_outlier(df, col))
     
 for col in df.columns:
     replace_with_thresholds(df, col)
+
+# Drop Outliers
+a = drop_outliers(df,'Insulin',th1=0.05, th3=0.9)
